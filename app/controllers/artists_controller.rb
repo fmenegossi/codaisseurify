8f649cc69e007918f9b8b5b06fdc500448917e49
Felipe Menegossi <felipe.menegossi@gmail.com>
2018-01-19 23:33:31 +0100

    Add artists controller. Add artist index and show views. Config to cascade delete

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,16 @@
 class ArtistsController < ApplicationController
+
+	def index
+		@artists = Artist.all
+	end
+
+	def show
+		@artist = Artist.find(params[:id])
+	end
+
+	private
+
+	def artist_params
+		params.required(:artist).permit(:name, :style, :image)
+	end
 end
