a89043e6e587da0a6b2c1a6a093cd4d501db809c
Felipe Menegossi <felipe.menegossi@gmail.com>
2018-01-19 22:48:10 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+bloodbath = Artist.create!(name: "Bloodbath", style: "Death Metal")
+immortal = Artist.create!(name: "Immortal", style: "Black Metal")
+vitas = Artist.create!(name: "Vitas", style: "Russian Pop")
+
+Song.create!([
+	{ title: "One by One", artist: immortal },
+	{ title: "Sons of Northern Darkness", artist: immortal },
+	{ title: "Eaten", artist: bloodbath },
+	{ title: "Cry My Name", artist: bloodbath },
+	{ title: "The 7th Element", artist: vitas },
+	{ title: "Extraterrestrial Friend", artist: vitas },
+	])
